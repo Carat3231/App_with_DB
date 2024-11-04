887feb7409173108328ebc1af00db645a8bec85d
Carat3231 <yuipovvv7@gmail.com>
2024-11-05 01:52:22 +0900

    first commit

---
+++ AppWithDB/DataBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AppWithDB
+{
+    internal class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source = CARAT; Initial Catalog = test_db; Integrated Security = True");
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed) { sqlConnection.Open(); }
+        }
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open) { sqlConnection.Close(); }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection; 
+        }
+    }
+}
